d29f23db4c29f2635068975c5564daa07c2e0a09
Rajgit1807 <rudrii1807@gmail.com>
2022-10-23 20:40:21 +0530

    Please look into my code

---
+++ Palindrome.js
@@ -0,0 +1,10 @@
+var str ="Rudraraj";
+var str1 ="";
+for(i = 0 ; i >= str.length -1 ; i--){
+       str1 += str[i];
+}
+if(str ==str1){
+    console.log(str , " is palindrome ");
+}else{
+    console.log(str , "is not palindrome");
+}
---
+++ PrimeNo.js
@@ -0,0 +1,13 @@
+ var num = 13;
+      let count=0;
+for(i = 0 ; i < count.length ; i++){
+    if(num % i === 0){
+        count++;
+    }
+ }
+ if(count===5){
+      console.log(num , "is prime" );
+ }else{
+     console.log(num , "is not  prime" );
+    }
+ 
